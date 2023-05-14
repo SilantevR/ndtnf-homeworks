f57ac1926dc0024ca95242a5574e1010e0709b41
craft <craft_89@inbox.ru>
2023-05-14 11:47:49 +0300

    homework_6

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -1,9 +1,18 @@
-import {of, from, timer, range} from 'rxjs';
+import { of, from, timer, range } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const o = range(0, 10)
+const o = range(0, 10);
 
 o.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+  next: (value: any) => console.log("Next:", value),
+  complete: () => console.log("Complete!"),
+  error: (error) => console.log("Error!", error),
+});
+
+const gitHub = ajax.getJSON(
+  "https://api.github.com/search/repositories?q=rxjs"
+);
+gitHub.subscribe((value) => console.log("Github", value));
+
+const gitLab = ajax.getJSON("https://gitlab.com/api/v4/projects?search=rxjs");
+gitLab.subscribe((value) => console.log("Gitlab", value));
